b9f8541614eecf042c171b750d26e07b164f3396
huangzc <540955198@qq.com>
2022-02-11 14:32:43 +0800

    只有logout和whoami才需要登录cookie校验

--- server/router.go
+++ server/router.go
@@ -20,33 +20,31 @@ func NewRouter() *gin.Engine {
 	g := r.Group("/api/v1")
 
 	g.POST("/auth/login", api.Login) // 登录后设置了session   user_id   与CurrentUser 中的user_id相呼应
+
+	// 成员管理
+	g.GET("/member", api.GetMember)
+	g.POST("/member/create", api.CreateMember)
+	g.GET("/member/list", api.GetMemberList)
+	g.POST("/member/update", api.UpdateMember)
+	g.POST("/member/delete", api.DeleteMember)
+	// 排课
+	g.POST("/course/create", api.CreateCourse)
+	g.GET("/course/get", api.GetCourse)
+
+	g.POST("/teacher/bind_course", api.BindCourse)
+	g.POST("/teacher/unbind_course", api.UnBindCourse)
+	g.GET("/teacher/get_course", api.GetTeacherCourse)
+	g.POST("/course/schedule", api.ScheduleCourse)
+
+	// 抢课
+	g.POST("/student/book_course")
+	g.GET("/student/course")
 	auth := g.Group("")
 	auth.Use(middleware.AuthRequired())
 	{
-		// 成员管理
-		auth.GET("/member", api.GetMember)
-		auth.POST("/member/create", api.CreateMember)
-		auth.GET("/member/list", api.GetMemberList)
-		auth.POST("/member/update", api.UpdateMember)
-		auth.POST("/member/delete", api.DeleteMember)
-
 		// 登录
 		auth.POST("/auth/logout", api.Logout)
-
 		auth.GET("/auth/whoami", api.Whoami)
-
-		// 排课
-		auth.POST("/course/create", api.CreateCourse)
-		auth.GET("/course/get", api.GetCourse)
-
-		auth.POST("/teacher/bind_course", api.BindCourse)
-		auth.POST("/teacher/unbind_course", api.UnBindCourse)
-		auth.GET("/teacher/get_course", api.GetTeacherCourse)
-		auth.POST("/course/schedule", api.ScheduleCourse)
-
-		// 抢课
-		auth.POST("/student/book_course")
-		auth.GET("/student/course")
 	}
 	return r
 }
