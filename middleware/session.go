b88ef7e4272e6501c0f27c87996b07799b896fb4
huangzc <540955198@qq.com>
2022-02-11 14:40:46 +0800

    session

--- middleware/session.go
+++ middleware/session.go
@@ -4,12 +4,13 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 // Session 初始化session
 func Session(secret string) gin.HandlerFunc {
-	//store := cookie.NewStore([]byte(secret))
-	store, _ := redis.NewStore(10, "tcp", "localhost:6379", "", []byte(secret))
+	redisAddr := os.Getenv("REDIS_ADDR")
+	store, _ := redis.NewStore(10, "tcp", redisAddr, "", []byte(secret))
 	//Also set Secure: true if using SSL, you should though
 	store.Options(sessions.Options{HttpOnly: true, MaxAge: 900, Path: "/"})
 	return sessions.Sessions("camp-seesion", store)
