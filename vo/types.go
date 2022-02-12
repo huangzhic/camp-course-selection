2b785c87cfa13ad5f414a13848f39d69febd4c8f
Roscky <943264235@qq.com>
2022-02-13 00:25:23 +0800

    调整错误码格式

--- vo/types.go
+++ vo/types.go
@@ -11,25 +11,25 @@ import "camp-course-selection/model"
 type ErrNo int
 
 const (
-	OK                 ErrNo = 0
-	ParamInvalid       ErrNo = 1  // 参数不合法
-	UserHasExisted     ErrNo = 2  // 该 Username 已存在
-	UserHasDeleted     ErrNo = 3  // 用户已删除
-	UserNotExisted     ErrNo = 4  // 用户不存在
-	WrongPassword      ErrNo = 5  // 密码错误
-	LoginRequired      ErrNo = 6  // 用户未登录
-	CourseNotAvailable ErrNo = 7  // 课程已满
-	CourseHasBound     ErrNo = 8  // 课程已绑定过
-	CourseNotBind      ErrNo = 9  // 课程未绑定过
-	PermDenied         ErrNo = 10 // 没有操作权限
-	StudentNotExisted  ErrNo = 11 // 学生不存在
-	CourseNotExisted   ErrNo = 12 // 课程不存在
-	StudentHasNoCourse ErrNo = 13 // 学生没有课程
-	StudentHasCourse   ErrNo = 14 // 学生有课程
+	OK                        ErrNo = 0
+	ParamInvalid              ErrNo = 1  // 参数不合法
+	UserHasExisted            ErrNo = 2  // 该 Username 已存在
+	UserHasDeleted            ErrNo = 3  // 用户已删除
+	UserNotExisted            ErrNo = 4  // 用户不存在
+	WrongPassword             ErrNo = 5  // 密码错误
+	LoginRequired             ErrNo = 6  // 用户未登录
+	CourseNotAvailable        ErrNo = 7  // 课程已满
+	CourseHasBound            ErrNo = 8  // 课程已绑定过
+	CourseNotBind             ErrNo = 9  // 课程未绑定过
+	PermDenied                ErrNo = 10 // 没有操作权限
+	StudentNotExisted         ErrNo = 11 // 学生不存在
+	CourseNotExisted          ErrNo = 12 // 课程不存在
+	StudentHasNoCourse        ErrNo = 13 // 学生没有课程
+	StudentHasCourse          ErrNo = 14 // 学生有课程
+	CourseHasExisted          ErrNo = 15 // 课程已经存在
+	StudentCannotBindOrUnBind ErrNo = 16 // 课程不能与学生进行绑定或解绑等的相关操作
 
-	CourseHasExisted          int = 15  // 课程已经存在
-	StudentCannotBindOrUnBind int = 16  // 课程不能与学生进行绑定或解绑等的相关操作
-	UnknownError              int = 255 // 未知错误
+	UnknownError ErrNo = 255 // 未知错误
 )
 
 type ResponseMeta struct {
