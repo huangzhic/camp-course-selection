f3d6556ee53ec5da0f830567420a0dc3517f67d1
huangzc <540955198@qq.com>
2022-02-17 12:54:52 +0800

    修复bug

--- middleware/session.go
+++ middleware/session.go
@@ -10,7 +10,7 @@ import (
 // Session 初始化session
 func Session(secret string) gin.HandlerFunc {
 	redisAddr := os.Getenv("REDIS_ADDR")
-	store, _ := redis.NewStore(10, "tcp", redisAddr, "", []byte(secret))
+	store, _ := redis.NewStore(10, "tcp", redisAddr, os.Getenv("REDIS_PW"), []byte(secret))
 	//Also set Secure: true if using SSL, you should though
 	store.Options(sessions.Options{HttpOnly: true, MaxAge: 900, Path: "/"})
 	return sessions.Sessions("camp-seesion", store)
